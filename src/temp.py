4b39ade1322e353402637e59c4a93afb20c4f4bd
Richard Min <richardmin97@gmail.com>
2018-05-24 13:49:47 -0700

    Initial Commit

---
+++ src/temp.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+import nibabel as nib
+import configparser
+import json
+
+
+# parse in config to see where to read the files in from
+config = configparser.ConfigParser()
+config.read('../config.ini')
+location = config['FILES']['Location']
+print(location)
