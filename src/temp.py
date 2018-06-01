49aa858fbef9cf0c2dd00df4ac7b225298e1827e
Richard Min <richardmin97@gmail.com>
2018-05-31 17:32:48 -0700

    Added test files

--- src/temp.py
+++ src/temp.py
@@ -3,7 +3,7 @@ import numpy as np
 import nibabel as nib
 import configparser
 import json
-
+from nilearn import plotting
 
 # parse in config to see where to read the files in from
 config = configparser.ConfigParser()
